78ed4bad43576459c9033fe63be4a4a1cbbed0ab
rosenrose <crazytempler@gmail.com>
2022-04-07 17:41:37 +0900

    refactor

--- responsive_tester.py
+++ responsive_tester.py
@@ -5,27 +5,44 @@ from webdriver_manager.chrome import ChromeDriverManager
 from math import ceil
 from pathlib import Path
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("excludeSwitches", ["enable-logging"])
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+class ResponsiveTester:
+  def __init__(self, urls):
+    self.urls = urls
+    self.options = webdriver.ChromeOptions()
+    self.options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.options)
+    self.driver.maximize_window()
+    self.widths = [480, 960, 1366, 1920]
+  
+  def prepare(self):
+    self.screenshots = Path("screenshots")
+    self.screenshots.mkdir(exist_ok=True)
 
-driver.get("https://nomadcoders.co")
-driver.maximize_window()
+  def take_screenshot(self, url):
+    self.driver.get(url)
+    BROWSER_HEIGHT = self.driver.get_window_size()["height"]
+    urlName = url.removeprefix("https://").removeprefix("http://").removesuffix("/").replace("/", "_").strip()
+    (urlFolder := self.screenshots / urlName).mkdir(exist_ok=True)
 
-widths = [480, 960, 1366, 1920]
-BROWSER_HEIGHT = driver.get_window_size()["height"]
-(screenshots := Path("screenshots")).mkdir(exist_ok=True)
-for png in screenshots.glob("size*.png"):
-  png.unlink()
+    for width in self.widths:
+      self.driver.set_window_size(width=width, height=BROWSER_HEIGHT)
+      scroll_height = self.driver.execute_script("""return document.body.scrollHeight;""")
+      total_sections = ceil(scroll_height / BROWSER_HEIGHT)
 
-for width in widths:
-  driver.set_window_size(width=width, height=BROWSER_HEIGHT)
-  scroll_height = driver.execute_script("""return document.body.scrollHeight;""")
-  total_sections = ceil(scroll_height / BROWSER_HEIGHT)
+      for section in range(total_sections):
+        self.driver.execute_script(f"""window.scrollTo(0, {section * BROWSER_HEIGHT});""")
+        time.sleep(0.5)
+        self.driver.save_screenshot(str(urlFolder / f"{width}_{section + 1}.png"))
 
-  for section in range(total_sections):
-    driver.execute_script(f"""window.scrollTo(0, {section * BROWSER_HEIGHT});""")
-    driver.save_screenshot(str(screenshots / f"size_{width}_{section + 1}.png"))
-    time.sleep(1)
+  def start(self):
+    self.prepare()
+    for url in self.urls:
+      self.take_screenshot(url);
+    self.finish()
 
-driver.quit()
+  def finish(self):
+    self.driver.quit()
+
+if __name__ == "__main__":
+  tester = ResponsiveTester(["https://nomadcoders.co", "https://google.com"])
+  tester.start()
