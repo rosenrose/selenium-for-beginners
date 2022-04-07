2ecfa203b07187b9b9e859aa8d3247bf45b692e4
rosenrose <crazytempler@gmail.com>
2022-04-07 16:28:46 +0900

    paginate

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 from pathlib import Path
 
 class GoogleKeywordScreenshooter:
-  def __init__(self, keyword, screenshots_dir):
+  def __init__(self, keyword, screenshots_dir, max_pages):
     self.keyword = keyword
     self.options = webdriver.ChromeOptions()
     self.options.add_experimental_option("excludeSwitches", ["enable-logging"])
     self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.options)
     self.screenshots_dir = Path(screenshots_dir)
+    self.max_pages = max_pages
   
   def prepare(self):
     self.screenshots_dir.mkdir(exist_ok=True)
@@ -26,33 +27,41 @@ class GoogleKeywordScreenshooter:
     search_bar = self.driver.find_element(By.CSS_SELECTOR, "input[type='text']")
     search_bar.send_keys(self.keyword)
     search_bar.send_keys(Keys.RETURN)
+    current_page = 1
 
-    try:
-      related_questions = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(), '관련 질문')]/../../../../..")))
-      self.driver.execute_script("""
-        console.log(arguments)
-        arguments[0].remove();
-      """, related_questions)
-    except:
-      pass
-
-    search_results = self.driver.find_elements(By.CSS_SELECTOR, "#search h1 + div > *")
-    # print(search_results)
-    for i, result in enumerate(search_results):
-      # title = result.find_element(By.CSS_SELECTOR, "h3")
-      # if title:
-      #   print(title.text)
+    while current_page <= self.max_pages:
       try:
-        result.screenshot(str(self.screenshots_dir / f"{self.keyword}_{i + 1:02}.png"))
+        related_questions = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(), '관련 질문')]/../../../../..")))
+        self.driver.execute_script("""
+          console.log(arguments)
+          arguments[0].remove();
+        """, related_questions)
       except:
         pass
+
+      search_results = self.driver.find_elements(By.CSS_SELECTOR, "#search h1 + div > *")
+      # print(search_results)
+      for i, result in enumerate(search_results):
+        # title = result.find_element(By.CSS_SELECTOR, "h3")
+        # if title:
+        #   print(title.text)
+        try:
+          result.screenshot(str(self.screenshots_dir / f"{self.keyword}_page{current_page}_{i + 1:02}.png"))
+        except:
+          pass
+      
+      pagination = self.driver.find_element(By.CSS_SELECTOR, "div[role='navigation'] table")
+      pages = pagination.find_elements(By.CSS_SELECTOR, "tr td")
+      pages[-1].find_element(By.CSS_SELECTOR, "a").click()
+      current_page += 1
+
     self.finish()
 
   def finish(self):
     self.driver.quit()
 
 if __name__ == "__main__":
-  domain_competitors = GoogleKeywordScreenshooter("buy domain", "screenshots")
-  python_competitors = GoogleKeywordScreenshooter("python book", "screenshots")
+  domain_competitors = GoogleKeywordScreenshooter("buy domain", "screenshots", 2)
+  python_competitors = GoogleKeywordScreenshooter("python book", "screenshots", 3)
   domain_competitors.start()
   python_competitors.start()
