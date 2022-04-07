5e8a8cacc2fd736c5a9cf4680ae62867da5aca2f
rosenrose <crazytempler@gmail.com>
2022-04-07 16:12:48 +0900

    cleaning code

--- main.py
+++ main.py
@@ -7,35 +7,52 @@ from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from pathlib import Path
 
-KEYWORD = "buy domain"
-(screenshots := Path("screenshots")).mkdir(exist_ok=True)
-for png in screenshots.iterdir():
-  png.unlink()
+class GoogleKeywordScreenshooter:
+  def __init__(self, keyword, screenshots_dir):
+    self.keyword = keyword
+    self.options = webdriver.ChromeOptions()
+    self.options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.options)
+    self.screenshots_dir = Path(screenshots_dir)
+  
+  def prepare(self):
+    self.screenshots_dir.mkdir(exist_ok=True)
+    for png in self.screenshots_dir.glob(f"{self.keyword}*.png"):
+      png.unlink()
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("excludeSwitches", ["enable-logging"])
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-driver.get("https://google.com")
+  def start(self):
+    self.prepare()
+    self.driver.get("https://google.com")
+    search_bar = self.driver.find_element(By.CSS_SELECTOR, "input[type='text']")
+    search_bar.send_keys(self.keyword)
+    search_bar.send_keys(Keys.RETURN)
 
-search_bar = driver.find_element(By.CSS_SELECTOR, "input[type='text']")
-search_bar.send_keys(KEYWORD)
-search_bar.send_keys(Keys.RETURN)
+    try:
+      related_questions = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(), '관련 질문')]/../../../../..")))
+      self.driver.execute_script("""
+        console.log(arguments)
+        arguments[0].remove();
+      """, related_questions)
+    except:
+      pass
 
-related_questions = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(), '관련 질문')]/../../../../..")))
-driver.execute_script("""
-  console.log(arguments)
-  arguments[0].remove();
-""", related_questions)
+    search_results = self.driver.find_elements(By.CSS_SELECTOR, "#search h1 + div > *")
+    # print(search_results)
+    for i, result in enumerate(search_results):
+      # title = result.find_element(By.CSS_SELECTOR, "h3")
+      # if title:
+      #   print(title.text)
+      try:
+        result.screenshot(str(self.screenshots_dir / f"{self.keyword}_{i + 1:02}.png"))
+      except:
+        pass
+    self.finish()
 
-search_results = driver.find_elements(By.CSS_SELECTOR, "#search h1 + div > *")
-for i, result in enumerate(search_results):
-  # title = result.find_element(By.CSS_SELECTOR, "h3")
-  # if title:
-  #   print(title.text)
-  try:
-    result.screenshot(f"screenshots/{KEYWORD}_{i + 1:02}.png")
-  except:
-    pass
+  def finish(self):
+    self.driver.quit()
 
-input()
-driver.quit()
+if __name__ == "__main__":
+  domain_competitors = GoogleKeywordScreenshooter("buy domain", "screenshots")
+  python_competitors = GoogleKeywordScreenshooter("python book", "screenshots")
+  domain_competitors.start()
+  python_competitors.start()
